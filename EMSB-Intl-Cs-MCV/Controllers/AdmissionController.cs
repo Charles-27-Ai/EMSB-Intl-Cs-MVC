fd4bb2173aa5ced7b35431f94c4918a5c18888bb
Charles Ai <kai@emsb.qc.ca>
2019-05-30 10:35:03 -0400

    Home page finished (gallery links)

---
+++ EMSB-Intl-Cs-MCV/Controllers/AdmissionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMSB_Intl_Cs_MCV.Controllers
+{
+    public class AdmissionController : Controller
+    {
+        // GET: Admission
+        public ActionResult Admission()
+        {
+            ViewBag.Message = "把从 Model 传来的信息通过算法分发到 View";
+            return View();
+        }
+
+        public ActionResult Step2()
+        {
+            return View();
+        }
+
+        public ActionResult Step3()
+        {
+            return View();
+        }
+    }
+}
